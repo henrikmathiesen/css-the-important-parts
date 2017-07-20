fa0a3792fba0bea25681359efb74bd81d4a0bdd9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-20 16:34:55 +0200

    -changed text

--- src/box-sizing/box-sizing.js
+++ src/box-sizing/box-sizing.js
@@ -1,7 +1,7 @@
 $(function () {
     
     var $contentBox03 = $('.ip-content-box-03');
-    console.log('.ip-content-box-03, width(), css speced width', $contentBox03.width());
+    console.log('.ip-content-box-03, width(), calculated width', $contentBox03.width());
     console.log('.ip-content-box-03, innerWidth(), calculated width with padding but not border', $contentBox03.innerWidth());
     console.log('.ip-content-box-03, outerWidth(), calculated width with padding and border', $contentBox03.outerWidth());
 
