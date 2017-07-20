fe82a3661f2dccf07702c5623f55bb5dc0921272
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-20 19:59:42 +0200

    -corrected bug -labbing with box-sizing

--- src/box-sizing/box-sizing.js
+++ src/box-sizing/box-sizing.js
@@ -1,12 +1,30 @@
 $(function () {
     
-    var $contentBox03 = $('.ip-content-box-03');
-    console.log('.ip-content-box-03, width(), calculated width', $contentBox03.width());
-    console.log('.ip-content-box-03, innerWidth(), calculated width with padding but not border', $contentBox03.innerWidth());
-    console.log('.ip-content-box-03, outerWidth(), calculated width with padding and border', $contentBox03.outerWidth());
+    console.log('--- content-box ---');
 
-    var $contentBox03Js = document.querySelector('.ip-content-box-03');
-    console.log('.ip-content-box-03 clientWidth, calculated width with padding but not border', $contentBox03Js.clientWidth);
-    console.log('.ip-content-box-03 offsetWidth, calculated width with padding and border', $contentBox03Js.offsetWidth);
+    var $contentBox03 = $('.js-content-box-03');
+    console.log('.ip-box-03, width(), calculated width', $contentBox03.width());
+    console.log('.ip-box-03, innerWidth(), calculated width with padding but not border', $contentBox03.innerWidth());
+    console.log('.ip-box-03, outerWidth(), calculated width with padding and border', $contentBox03.outerWidth());
+
+    var $contentBox03Js = document.querySelector('.js-content-box-03');
+    console.log('.ip-box-03, clientWidth, calculated width with padding but not border', $contentBox03Js.clientWidth);
+    console.log('.ip-box-03, offsetWidth, calculated width with padding and border', $contentBox03Js.offsetWidth);
+
+    console.log('--- /content-box ---');
+
+    
+    console.log('--- border-box ---');
+
+    var $borderBox03 = $('.js-border-box-03');
+    console.log('.ip-box-03, width(), calculated width', $borderBox03.width());
+    console.log('.ip-box-03, innerWidth(), calculated width with padding but not border', $borderBox03.innerWidth());
+    console.log('.ip-box-03, outerWidth(), calculated width with padding and border', $borderBox03.outerWidth());
+
+    var $borderBox03Js = document.querySelector('.js-border-box-03');
+    console.log('.ip-box-03, clientWidth, calculated width with padding but not border', $borderBox03Js.clientWidth);
+    console.log('.ip-box-03, offsetWidth, calculated width with padding and border', $borderBox03Js.offsetWidth);
+
+    console.log('--- /border-box ---');
 
 });
