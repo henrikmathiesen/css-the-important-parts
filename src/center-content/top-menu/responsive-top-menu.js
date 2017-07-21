2cf26005e39c2dd08fc8db03d5e49cda82889b17
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-21 16:28:00 +0200

    -design

--- src/center-content/top-menu/responsive-top-menu.js
+++ src/center-content/top-menu/responsive-top-menu.js
@@ -1,6 +1,13 @@
 $(function () {
-    
-    var $nav = $('.js-ip-nav');
 
-    console.log($nav);
+    var $nav = $('.js-ip-nav');
+    var $icon = $nav.find('.fa-times');
+    var $lisLinks = $nav.find('li:not(li:first-child)');
+    var isExpanded = false;
+
+    $icon.click(function () { 
+        $lisLinks.css('display', !isExpanded ? 'list-item' : 'none');
+        isExpanded = !isExpanded;
+    });
+
 });
