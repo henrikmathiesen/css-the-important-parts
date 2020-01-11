0579e5810ec1b3cc19ead0db475d9bfd2ed84381
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-01-11 09:57:01 +0100

    -animation

--- src/center-content/top-menu/responsive-top-menu.js
+++ src/center-content/top-menu/responsive-top-menu.js
@@ -3,11 +3,12 @@ $(function () {
     FastClick.attach(document.body);
 
     var $nav = $('.js-ip-nav');
-    var $icon = $nav.find('.fa-times');
+    var $icon = $nav.find('.ip-menu-container');
     var $menuItems = $nav.find('.js-ip-nav-item');
 
     var toggleMenu = function () { 
         $menuItems.toggleClass('ip-show-for-md-up');
+        $icon.find('.ip-menu-icon').toggleClass('ip-menu-open');
     };
 
     $icon.click(toggleMenu);
