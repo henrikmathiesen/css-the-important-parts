bb3f19821d8f40ed1349a2ce8efc99247bef9263
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-07-22 17:28:59 +0200

    -transitionend callback

--- src/center-content/top-menu/responsive-top-menu.js
+++ src/center-content/top-menu/responsive-top-menu.js
@@ -6,7 +6,7 @@ $(function () {
     var $icon = $nav.find('.ip-menu-container');
     var $menuItems = $nav.find('.js-ip-nav-item');
 
-    var toggleMenu = function () { 
+    var toggleMenu = function () {
         $menuItems.toggleClass('ip-show-for-md-up');
         $icon.find('.ip-menu-icon').toggleClass('ip-menu-open');
     };
@@ -25,4 +25,15 @@ $(function () {
         toggleMenu();
     });
 
+    //
+    // Testing transition callback (X button shown on mobile)
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions
+
+    $('.ip-menu-icon').on('transitionend', () => {
+        console.log('transitionend jquery');
+    });
+
+    document.querySelector('.ip-menu-icon').addEventListener('transitionend', () => { 
+        console.log('transitionend vanilla js');
+    });
 });
